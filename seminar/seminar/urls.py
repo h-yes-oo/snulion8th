a4473adcb4f513efc1cf6f788d852231a7e1a3fe
jieun <wldms5764@gmail.com>
2020-05-05 12:27:01 +0900

    1-django-seminar CRUD

--- seminar/seminar/urls.py
+++ seminar/seminar/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 import feedpage.views
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', feedpage.views.index, name='index'),   
+    path('', feedpage.views.index, name='index'),
+    path('feeds/', include('feedpage.urls')),
 ]
