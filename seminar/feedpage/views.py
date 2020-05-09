a6265e9d6b2b988f32842ce658c2d335b9957b81
혜민 <lhm0710@naver.com>
2020-05-09 17:03:09 +0900

    1-django-seminar CRUD

--- seminar/feedpage/views.py
+++ seminar/feedpage/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from .models import Feed
+from django.shortcuts import redirect
 
 # Create your views here.
-def index(request):
-    return render(request, 'feedpage/index.html')
+def index(request): # 원래 있던 index 함수 수정
+    if request.method == 'GET':
+        feeds = Feed.objects.all()
+        # Feed object를 불러와서 feeds에 다 저장
+        return render(request, 'feedpage/index.html', {'feeds': feeds})
+        # 이 feeds를 feedpage에 넘겨주겠다
+    elif request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        Feed.objects.create(title=title,content=content)
+        return redirect('/feeds')
+
+def new(request):
+    return render(request, 'feedpage/new.html')
+
+def show(request, id):
+    feed=Feed.objects.get(id=id)
+    return render(request, 'feedpage/show.html', {'feed':feed})
+
+def delete(request, id):
+    feed = Feed.objects.get(id=id)
+    feed.delete()
+    return redirect('/feeds')
