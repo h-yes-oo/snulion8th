8cedfc24c5ea91c997e0d9147eda1a5ecb8d61d4
Eunjoo <lynn0506@snu.ac.kr>
2020-05-09 15:51:11 +0900

    1-django-seminar CRUD

--- seminar/feedpage/views.py
+++ seminar/feedpage/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from .models import Feed # 추가. (참고: .models == feedpage.models)
+from django.shortcuts import redirect
+
 # feedpage/view.py
 ...
 def index(request):
-    return render(request, 'feedpage/index.html')
-    
+    if request.method == 'GET':
+        feeds = Feed.objects.all()
+        return render(request, 'feedpage/index.html', {'feeds': feeds})
+
+    elif request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        Feed.objects.create(title=title, content=content)
+        return redirect('/feeds')
+
+def new(request):
+    return render(renquest, 'feedpage/new.html')
+
+def delete(request, id):
+    feed = Feed.objects.get(id=id)
+    feed.delete()
+    return redirect('/feeds')
+
+def show(request, id):
+    feed=Feed.objects.get(id=id)
+    return render(request, 'feedpage/show.html', {'feed': fedd})
--- seminar/seminar/urls.py
+++ seminar/seminar/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import feedpage.views 
+from django.conf.urls import include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', feedpage.views.index, name='index'),
-]
+    path('feeds/', include('feedpage.urls')), 
+    # 항상 끝에도 쉼표를 붙여 줍시다 
+] 
