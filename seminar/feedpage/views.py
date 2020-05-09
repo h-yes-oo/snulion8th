b216235cf03dad803cc7d37262302b6a068cb260
jieun <wldms5764@gmail.com>
2020-05-09 16:13:10 +0900

    temp

--- seminar/feedpage/views.py
+++ seminar/feedpage/views.py
@@ -1,25 +1,8 @@
 from django.shortcuts import render
 from .models import Feed 
-from django.shortcuts import redirect
 
 def index(request):
-    if request.method == 'GET': # index
-        feeds = Feed.objects.all()
-        return render(request, 'feedpage/index.html', {'feeds': feeds})
-    elif request.method == 'POST': # create(form을 이용하여 submit한 형태) 
-        title = request.POST['title']
-        content = request.POST['content']
-        Feed.objects.create(title=title, content=content)
-        return redirect('/feeds') 
+    feeds = Feed.objects.all()
+    return render(request, 'feedpage/index.html', {'feeds': feeds})
 
-def new(request):
-    return render(request, 'feedpage/new.html')
 
-def show(request,id):
-    feed=Feed.objects.get(id=id)
-    return render(request,'feedpage/show.html',{'feed':feed})
-
-def delete(request, id):
-    feed = Feed.objects.get(id=id)
-    feed.delete()
-    return redirect('/feeds')
