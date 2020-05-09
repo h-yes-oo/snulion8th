d1ec88bc5ebc23221d4d9d4f6abb8d1c536f8223
kyungmin <kyungmin980416@gmail.com>
2020-05-09 17:11:06 +0900

    1-django-seminar CRUD

--- seminar/feedpage/models.py
+++ seminar/feedpage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Feed(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    def update_date(self):
+        self.updated_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
