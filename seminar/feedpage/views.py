d0bae39754a5021db58530004942e6df96903a92
혜민 <lhm0710@naver.com>
2020-05-15 04:12:36 +0900

    edit finished

--- seminar/feedpage/views.py
+++ seminar/feedpage/views.py
@@ -19,10 +19,21 @@ def new(request):
     return render(request, 'feedpage/new.html')
 
 def show(request, id):
-    feed=Feed.objects.get(id=id)
-    return render(request, 'feedpage/show.html', {'feed':feed})
+    if request.method == 'GET':
+        feed=Feed.objects.get(id=id)
+        return render(request, 'feedpage/show.html', {'feed':feed})
+    elif request.method == 'POST':
+        feed = Feed.objects.get(id=id)
+        feed.title = request.POST['title']
+        feed.content = request.POST['content']
+        feed.save()
+        return render(request, 'feedpage/show.html', {'feed':feed})
 
 def delete(request, id):
     feed = Feed.objects.get(id=id)
     feed.delete()
     return redirect('/feeds')
+
+def edit(request, id):
+    feed=Feed.objects.get(id=id)
+    return render(request, 'feedpage/edit.html', {'feed':feed})
