8f9a6daac93c164dd394c12b44c0f309d03a46ce
kjw7953 <kjw7953@github.com>
2020-05-14 15:43:11 +0900

    1-django-seminar CRUD

--- seminar/feedpage/views.py
+++ seminar/feedpage/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from .models import Feed
+from django.shortcuts import redirect
 
 def index(request):
-    return render(request, 'feedpage/index.html')
+    if request.method == 'GET':
+        feeds = Feed.objects.all()
+        return render(request, 'feedpage/index.html', {'feeds': feeds})
+    elif request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        Feed.objects.create(title=title, content=content)
+        return redirect('/feeds')
+
+def new(request):
+    return render(request, 'feedpage/new.html')
+
+def show(request, id):
+    feed = Feed.objects.get(id=id)
+    return render(request, 'feedpage/show.html', {'feed':feed})
+
+def delete(request, id):
+    feed = Feed.objects.get(id=id)
+    feed.delete()
+    return redirect('/feeds')
--- seminar/seminar/urls.py
+++ seminar/seminar/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import feedpage.views
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', feedpage.views.index, name='index'),
+    path('feeds/', include('feedpage.urls')),
 ]
