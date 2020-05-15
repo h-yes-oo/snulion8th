f55786e89124d7f20683d7870b28b5a00de51657
Minji Kim <minji1004y@gmail.com>
2020-05-16 03:13:01 +0900

    feedbback update

--- seminar/feedpage/urls.py
+++ seminar/feedpage/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('new/', views.new, name='new'),
     path('<int:id>/', views.show, name='show'),
-    path('<int:id>/edit', views.edit, name='edit'),
+    path('<int:id>/edit/', views.edit, name='edit'),
     path('<int:id>/update', views.update, name='update'),
-    path('<int:id>/delete', views.delete, name='delete'),
+    path('<int:id>/delete/', views.delete, name='delete'),
 ]
--- seminar/feedpage/views.py
+++ seminar/feedpage/views.py
@@ -11,50 +11,30 @@ def index(request):
     elif request.method == 'POST':
         title = request.POST['title']
         content = request.POST['content']
-        new_feed = Feed(title=title, content=content)
-        new_feed.save()
+        Feed.objects.create(title=title, content=content)
         return redirect('/feeds')
 
 
 def new(request):
     return render(request, 'feedpage/new.html')
 
+
 def show(request, id):
     feed = Feed.objects.get(id=id)
     return render(request, 'feedpage/show.html', {'feed':feed})
 
+
 def edit(request, id):
     feed = Feed.objects.get(id=id)
     return render(request, 'feedpage/edit.html', {'feed':feed})
-    #if request.method == 'POST':
-    #    title = request.POST['title']
-    #    content = request.POST['content']
-    #    edit_feed = Feed(title=title, content=content)
-    #   edit_feed.save()
-    #return redirect('/feeds')
-    #return redirect('/feeds')
-    #return render(request, 'feedpage/edit.html', {'feed':feed})
+
 
 def update(request, id):
     feed = Feed.objects.get(id=id)
-    if request.method == 'POST':
-        feed.title = request.POST['title']
-        feed.content = request.POST['content']
-        feed.save()
+    title = request.POST['title']
+    content = request.POST['content']
+    Feed.objects.update(title=title, content=content)
     return redirect('/feeds')
-'''
-def update(request, id):
-    feed = Feed.objects.get(id=id)
-    #if request.method == 'GET':
-    #    feeds = Feed.objects.all()
-    #    return render(request, 'feedpage/index.html', {'feeds': feeds})
-    if request.method == 'POST':
-        title = request.POST['title']
-        content = request.POST['content']
-        update_feed = Feed(title=title, content=content)
-        update_feed.save()
-        return redirect('/feeds') -->
-'''
 
 
 def delete(request, id):
