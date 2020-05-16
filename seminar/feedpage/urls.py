0a7f38627fd42406a0b786ff7c7eb85d225355d9
Jun <sungjun1703@snu.ac.kr>
2020-05-16 10:05:11 +0900

    1-django-seminar project seminar created

--- seminar/feedpage/urls.py
+++ seminar/feedpage/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('new/', views.new, name='new'),
     path('<int:id>/', views.show, name='show'),
-    path('<int:id>/delete', views.delete, name='delete'),
+    path('<int:id>/delete/', views.delete, name='delete'),
+    path('<int:id>/edit/', views.edit, name='edit'),
 ]
