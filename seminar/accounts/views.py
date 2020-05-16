76f559e835472f1ac64a5675a8d5bfd8f21e30f2
yeongjun <dudwns051230@gmail.com>
2020-05-16 16:46:34 +0900

    seminar ggeut

--- seminar/accounts/views.py
+++ seminar/accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.shortcuts import redirect
 
 # Create your views here.
+
+def signup(request):
+    if request.method  == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+            auth.login(request, user)
+            return redirect('/feeds')
+    return render(request, 'accounts/signup.html')
+
--- seminar/seminar/settings.py
+++ seminar/seminar/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'feedpage.apps.FeedpageConfig',
-    'accounts.apps.AccountsConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = "/feeds/"
