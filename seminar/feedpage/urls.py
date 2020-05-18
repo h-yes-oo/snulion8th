9c6ff6aed57e22a1330f36f668ef2cd8dc5a7f6e
kjw7953 <kjw7953@github.com>
2020-05-18 21:21:39 +0900

    Revert "2-CRUD-homework"
    This reverts commit f3173ac490368afbd16ac04c8438be2372a6c3d4.

--- seminar/feedpage/urls.py
+++ seminar/feedpage/urls.py
@@ -4,22 +4,8 @@ from feedpage import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('new/', views.new, name='new'),
-<<<<<<< HEAD
-<<<<<<< HEAD
     path('<int:id>/', views.show, name='show'),
     path('<int:id>/delete/', views.delete, name='delete'),
     path('<int:id>/edit/', views.edit, name='edit'),
     path('<int:id>/update/', views.update, name='update'),
-    
-=======
-=======
->>>>>>> parent of f3173ac... 2-CRUD-homework
-    path('<int:id>', views.show, name='show'),
-    path('<int:id>/delete', views.delete, name='delete'),
-    path('<int:id>/edit', views.edit, name='edit'),
-    path('<int:id>/editApply', views.editApply, name='editApply'),
-<<<<<<< HEAD
->>>>>>> parent of f3173ac... 2-CRUD-homework
-=======
->>>>>>> parent of f3173ac... 2-CRUD-homework
 ]
