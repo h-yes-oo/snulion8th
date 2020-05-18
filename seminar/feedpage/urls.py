55e86db3baf9347fef2c6fc2a4cdb1eea5c716c3
kjw7953 <kjw7953@github.com>
2020-05-18 21:19:31 +0900

    for revert1

--- seminar/feedpage/urls.py
+++ seminar/feedpage/urls.py
@@ -5,15 +5,21 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('new/', views.new, name='new'),
 <<<<<<< HEAD
+<<<<<<< HEAD
     path('<int:id>/', views.show, name='show'),
     path('<int:id>/delete/', views.delete, name='delete'),
     path('<int:id>/edit/', views.edit, name='edit'),
     path('<int:id>/update/', views.update, name='update'),
     
 =======
+=======
+>>>>>>> parent of f3173ac... 2-CRUD-homework
     path('<int:id>', views.show, name='show'),
     path('<int:id>/delete', views.delete, name='delete'),
     path('<int:id>/edit', views.edit, name='edit'),
     path('<int:id>/editApply', views.editApply, name='editApply'),
+<<<<<<< HEAD
+>>>>>>> parent of f3173ac... 2-CRUD-homework
+=======
 >>>>>>> parent of f3173ac... 2-CRUD-homework
 ]
