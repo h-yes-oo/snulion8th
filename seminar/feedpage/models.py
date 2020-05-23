23b5debaa25c5d94763533fcef4205314ff6ba84
Jiwon <a0000@Jiwonui-MacBook.local>
2020-05-23 17:46:40 +0900

    4-1:N, M:N

--- seminar/feedpage/models.py
+++ seminar/feedpage/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
-
+from faker import Faker
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Feed(models.Model):
@@ -8,6 +9,9 @@ class Feed(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey(User, null=True, on_delete= models.CASCADE)
+    like_users = models.ManyToManyField(User, blank=True, related_name='like_feeds', through='Like')
+
 
     def update_date(self):
         self.updated_at = timezone.now()
@@ -15,3 +19,20 @@ class Feed(models.Model):
     
     def __str(self):
         return self.title
+
+
+class FeedComment(models.Model):
+    content = models.TextField()
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
--- seminar/feedpage/views.py
+++ seminar/feedpage/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from .models import Feed
+from .models import Feed, FeedComment, Like
 from django.shortcuts import redirect
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your views here.
+
+
 def index(request):
     if request.method == 'GET':
         feeds = Feed.objects.all()
@@ -10,20 +13,24 @@ def index(request):
     elif request.method == 'POST':
         title = request.POST['title']
         content = request.POST['content']
-        Feed.objects.create(title=title, content=content)
+        feed = Feed.objects.create(title=title, content=content, author=request.user)
         return redirect('/feeds/%d/' %feed.id)#이건 get방식으로 url을 가서 다시 index로 돌아옴
+
 
 def new(request):
     return render(request, 'feedpage/new.html')
+
 
 def show(request,id):
     feed=Feed.objects.get(id=id)
     return render(request, 'feedpage/show.html',{'feed':feed})#{'feed':feed}가 무슨의미?
 
+
 def delete(request, id):
     feed = Feed.objects.get(id=id)
     feed.delete()
     return redirect('/feeds')
+
 
 def edit(request, id):
     if request.method == 'POST':
@@ -37,3 +44,24 @@ def edit(request, id):
 
     feed = Feed.objects.get(id=id)
     return render(request, 'feedpage/edit.html',{'feed':feed})
+
+
+def create_comment(request, id):
+    content = request.POST['content']
+    FeedComment.objects.create(feed_id=id, content=content )
+    return redirect('/feeds')
+
+
+def delete_comment(request, id, cid):
+    c = FeedComment.objects.get(id=cid)
+    c.delete()
+    return redirect('/feeds')
+
+def feed_like(request, pk):
+    feed = Feed.objects.get(id = pk)
+    like_list = feed.like_set.filter(user_id = request.user.id)
+    if like_list.count() > 0:
+        feed.like_set.get(user_id = request.user.id).delete()
+    else:
+        Like.objects.create(user_id = request.user.id, feed_id = feed.id)
+    return redirect('/feeds')
