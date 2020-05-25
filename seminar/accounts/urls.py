71e7ea9b791282c9fe6cccba94699dce642b0d97
sangwon <born9507@gmail.com>
2020-05-25 15:57:51 +0900

    팔로우 취소 구현

--- seminar/accounts/urls.py
+++ seminar/accounts/urls.py
@@ -5,7 +5,7 @@ from accounts import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('edit/', views.edit, name='mypage'),
-    path('<int:pk>/follow/', views.follow_manager, name='follow'),  ##<-추가
+    path('<int:pk>/follow/', views.follow_manager, name='follow'),
     # path('login/', views.login, name='login'),
     # path('logout/', views.logout, name='logout'),
 ]
