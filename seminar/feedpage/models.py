4298db4cdd989028851b722c4bf085b4173903eb
Hwan <tjsgkrtks012@naver.com>
2020-05-26 15:59:52 +0900

    1st review ended

--- seminar/feedpage/models.py
+++ seminar/feedpage/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+from faker import Faker
 
-# Create your models here.
+
+class Feed(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    def update_date(self):  # 나중에 수정할 때 사용
+        self.updated_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+    def seed(count):
+        myfake = Faker('ko_KR')
+        for i in range(count):
+            Feed.objects.create(
+                title=myfake.bs(),
+                content=myfake.text()
+            )
--- seminar/seminar/urls.py
+++ seminar/seminar/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import feedpage.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', feedpage.views.index, name='index')
+    path('', feedpage.views.index, name='index'),
+    path('feeds/', include('feedpage.urls')),
 ]
