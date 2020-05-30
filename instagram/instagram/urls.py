65f37c99f89d7e8427a8002aaa5d3e549610d265
daram <daram@dahyoungui-MacBook-Pro.local>
2020-05-30 18:00:37 +0900

    django-instagram project 2nd try

--- instagram/instagram/urls.py
+++ instagram/instagram/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import feedpage.views
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', feedpage.views.index, name='index'),
-]
+    path('feeds/', include('feedpage.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
