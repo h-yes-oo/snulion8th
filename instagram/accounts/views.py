f53a13fd29ed2745301fce1b0f01555d4d39e3f9
Eunjoo <lynn0506@snu.ac.kr>
2020-05-31 00:22:59 +0900

    instagram login & feed & comment & like complete

---
+++ instagram/accounts/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
+from .models import Profile
+from django.shortcuts import redirect
+
+def signup(request):
+    if request.method  == 'POST':
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        username = request.POST['username']
+        
+        if password1 != password2:
+            res_data = {}
+            res_data['error'] = "비밀번호가 일치하지 않습니다" 
+            return render(request, 'accounts/signup.html', res_data)
+        else:
+            user = User.objects.create_user(username=username,password=password1)
+            auth.login(request, user)
+            Profile.objects.filter(user = user).update(email = request.POST['email'],\
+                            birthday = request.POST['birthday'])
+            return redirect('/feeds')
+
+    elif request.method == 'GET':
+        return render(request, 'accounts/signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username = username, password = password)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/feeds')
+        else:
+            return render(request, 'accounts/login.html')
+            # , 'error':'username or password is incorrect')
+
+    elif request.method == 'GET':
+        return render(request, 'accounts/login.html')
+
+def logout(request):
+    return render(request, 'accounts/logged_out.html')
+
+def profile_edit(request, id):
+    if request.method == 'POST':
+        user = User.objects.get(id = id)
+        password = request.POST['password']
+
+        if user.check_password(password):
+            Profile.objects.filter(user = user).update(email = request.POST['email'], \
+                birthday = request.POST['birthday'])
+            return redirect('/feeds')
+        else:   
+            res_data = {}
+            res_data['error'] = "비밀번호가 일치하지 않습니다" 
+            return render(request, 'accounts/profile_edit.html', res_data)
+
+    elif request.method == 'GET':
+        return render(request, 'accounts/profile_edit.html')
+
---
+++ instagram/feedpage/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render
+from .models import Feed, FeedComment, Like, CommentLike
+from django.shortcuts import redirect 
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        content = request.POST['content']
+        photo =  request.FILES.get('photo', False) #사진 field가 비어있어도 되도록! 
+        Feed.objects.create(content=content, author= request.user, photo=photo)
+        return redirect('/feeds')
+    
+    feeds = Feed.objects.all()
+    return render(request, 'feedpage/index.html', {'feeds':feeds})
+
+def new(request):
+    return render(request, 'feedpage/new.html')
+
+def delete(request, id):
+    feed = Feed.objects.get(id=id)
+    feed.delete()
+    return redirect('/feeds')
+
+def show(request, id):
+    feed=Feed.objects.get(id=id)
+    if request.method == 'POST':
+        feed.content = request.POST['content']
+        feed.photo = request.FILES.get('photo', False)
+        feed.save()
+        # Feed.objects.filter(id=id).update(content=feed.content, photo=feed.photo)
+        # -> save로 하면 저장이되고, filter로 update하면 반영이 안되는 이유는...????ㅠ
+        
+    return render(request, 'feedpage/show.html', {'feed': feed})
+
+def edit(request, id):
+    feed = Feed.objects.get(id=id)
+    return render(request, 'feedpage/edit.html', {'feed': feed})
+
+def create_comment(request, id):
+    content = request.POST.get('content')
+    FeedComment.objects.create(feed_id=id, content=content, author=request.user)
+    return redirect('/feeds')
+
+def delete_comment(request, id, cid):
+    c = FeedComment.objects.get(id=cid)
+    c.delete()
+    return redirect('/feeds')
+
+def feed_like(request, pk):
+    feed = Feed.objects.get(id=pk)
+    like_list = feed.like_set.filter(user_id = request.user.id)
+    if like_list.count() > 0:
+        feed.like_set.get(user_id = request.user.id).delete()
+    else:
+        if request.user.id:
+            Like.objects.create(user_id = request.user.id, feed_id = feed.id)
+    return redirect ('/feeds')
+
+def comment_like(request, id, cid):
+    comment = FeedComment.objects.get(id=cid)
+    like_list = comment.commentlike_set.filter(user_id = request.user.id)
+    if like_list.count() > 0:
+        comment.commentlike_set.get(user_id = request.user.id).delete()
+    else:
+        if request.user.id:
+            CommentLike.objects.create(user_id = request.user.id, comment_id = comment.id)
+    return redirect ('/feeds')
