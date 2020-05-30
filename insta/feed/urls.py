d8c942532fb6a71de1aa19033385580846f08696
혜민 <lhm0710@naver.com>
2020-05-30 17:59:37 +0900

    seminar and instaHW

---
+++ insta/feed/urls.py
@@ -0,0 +1,12 @@
+# seminar/feedpage/urls.py
+from django.urls import path
+from feed import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('new/', views.new, name='new'),
+    path('<int:id>/', views.show, name='show'),
+    path('<int:id>/delete', views.delete, name='delete'),
+    path('<int:id>/comments/', views.create_comment, name='create_comment'),
+    path('<int:id>/comments/<int:cid>/', views.delete_comment, name='delete_comment'),
+]
