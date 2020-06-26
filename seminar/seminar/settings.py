ab8384018d2f9cc4b615897016b38d3fd20a419a
Hyesoo KIM <43427306+hyesoo5115@users.noreply.github.com>
2020-06-26 13:27:05 +0900

    Update settings.py

--- seminar/seminar/settings.py
+++ seminar/seminar/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'ko'
+LANGUAGE_CODE = 'kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
