1bb884d9db9d0efe64c1fac1319ce2c4f852caab
kjw7953 <uis7953@naver.com>
2020-08-28 13:21:57 +0900

    tutorial CBV before using mixins

---
+++ tuto/snippets/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
